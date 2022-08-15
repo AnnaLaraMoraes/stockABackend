7abdec03ed4face422fd9369f969ea10a35b576d
Anna Lara Moraes Caixeta <annalara1426@gmail.com>
2022-08-15 11:44:11 +0200

    fix firebase-config

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -4,7 +4,7 @@ const serviceAccount = {
   type: process.env.TYPE,
   project_id: process.env.PROJECTID,
   private_key_id: process.env.PRIVATEKEYID,
-  private_key: process.env.PRIVATEKEY,
+  private_key: process.env.PRIVATEKEY.replace(/\\n/g, '\n'),
   client_email: process.env.CLIENTEMAIL,
   client_id: process.env.CLIENTID,
   auth_uri: process.env.AUTHURI,
