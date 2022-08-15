06c8fde42e74befb549aba195b794d9224c54ec6
Anna Lara Moraes Caixeta <annalara1426@gmail.com>
2022-08-15 11:52:39 +0200

    fix

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,7 +2,9 @@ import admin from '../config/firebase-config';
 
 class Middleware {
   async decodeToken(req, res, next) {
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers.authorization
+      ? req.headers.authorization.split(' ')[1]
+      : '';
     try {
       const decodeValue = await admin.auth().verifyIdToken(token);
       if (decodeValue) {
